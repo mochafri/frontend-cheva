fe0667573c877b479934ac19d6f8917afb715a7a
mochafri <afrizalmuhammad656@gmail.com>
2024-07-12 23:29:41 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#A8151C',
+        'dark': '#640D11'
+      },
+    },
+  },
+  plugins: [],
+}
+
